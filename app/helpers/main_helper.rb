0587e4fa5200b84d0f6c5559edc1db04c02a8a24
Scott Lederer <scott@bit.ly>
2011-09-20 17:20:48 -0400

    swapped gt/lt signs; new messages

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -2,16 +2,16 @@ module MainHelper
 
   def dead_msg(time)
     return case
-    when time > 2.hours.ago
-      'NO WAY'
-    when time > 6.hours.ago
-      'EXTREMELY UNLIKELY'
-    when time > 24.hours.ago
-      'DOUBTFUL'
-    when time > 3.days.ago
-      'POSSIBLY'
+    when time < 2.hours.ago
+      'HIGHLY UNLIKELY'
+    when time < 6.hours.ago
+      'DUBIOUS'
+    when time < 24.hours.ago
+      "LET'S SAY... NO"
+    when time < 3.days.ago
+      'ARGUABLY'
     else
-      'CALL THE COPS?!'
+      'CALL THE COPS!!'
     end
   end
 
