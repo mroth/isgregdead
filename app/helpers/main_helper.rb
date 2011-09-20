16956686697cbc767da84d29b0568f3f3c1677ea
Scott Lederer <scott@bit.ly>
2011-09-20 17:36:57 -0400

    restored gt's to what they are a few min.ago

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -2,13 +2,13 @@ module MainHelper
 
   def dead_msg(time)
     return case
-    when time < 2.hours.ago
+    when time > 2.hours.ago
       'HIGHLY UNLIKELY'
-    when time < 6.hours.ago
+    when time > 6.hours.ago
       'DUBIOUS'
-    when time < 24.hours.ago
+    when time > 24.hours.ago
       "LET'S SAY... NO"
-    when time < 3.days.ago
+    when time > 3.days.ago
       'ARGUABLY'
     else
       'CALL THE COPS!!'
